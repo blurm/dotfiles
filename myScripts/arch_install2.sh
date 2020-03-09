7e29f6ba0116e1f02df2e816d3e71611f3600dc7
Damon Ma <damon.ma.iphone@gmail.com>
2020-03-09 10:17:05 +0800

    remove theme copy

--- myScripts/arch_install2.sh
+++ myScripts/arch_install2.sh
@@ -1,5 +1,4 @@
 ## Install oh-my-zsh plugin and other terminal tools {
     echo '---------------------- zsh-syntax-highlighting ---------------------'
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    cp ~/git4install/myconfigs/.oh-my-zsh/themes/damon.zsh-theme ~/.oh-my-zsh/themes/
 ## }
