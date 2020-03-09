2c9b8fca6c54a5452d01e9a5d357b4457b408801
Damon Ma <damon.ma.iphone@gmail.com>
2020-03-09 10:18:06 +0800

    Add zsh syntax highlighting

--- myScripts/arch.sh
+++ myScripts/arch.sh
@@ -250,4 +250,6 @@ echo '------------------------ calibre ------------------------------'
     yay -S zsh
     # on my zsh
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    echo '---------------------- zsh-syntax-highlighting ---------------------'
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 ## }
