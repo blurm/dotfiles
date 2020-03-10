382e7685acd0f22443fbf83fcc1307c9578951c9
damon <blurm@126.com>
2020-03-10 12:17:35 +0800

    add test.sh

---
+++ myScripts/test.sh
@@ -0,0 +1,234 @@
+#!/bin/bash
+
+#####################################
+# System install script for Arch    #
+#####################################
+
+
+### 安装软件
+    mkdir -p ~/git4install
+
+## Xorg & DM {
+    sudo pacman -Sy xorg-server xorg-xinit
+    sudo pacman -S lightdm lightdm-webkit2-greeter
+    sudo systemctl enable lightdm
+## }
+
+## Basic Tools {
+    echo '------------ base devel  -------------'
+    sudo pacman -S base-devel
+    sudo pacman -S linux-headers
+    sudo pacman -S cmake
+
+    echo '------------ git -------------'
+    sudo pacman -S  git
+    git config --global user.name "damon"
+    git config --global user.email "blurm@126.com"
+
+    echo '------------------------ yay(from archlinuxcn) ------------------------------'
+    sudo pacman -S yay
+    #cd git4install
+    #git clone https://aur.archlinux.org/yay.git
+    #cd yay
+    #makepkg -si
+
+## }
+
+## Internet {
+    echo '----------------- google-chrome, chromium -------------------'
+    yay -S chromium
+## }
+
+echo '------------------------ calibre ------------------------------'
+
+
+## System tools {
+    echo '----------------- System Tools -------------------'
+    echo '----------------- htop -------------------'
+    #echo '----------------- Input Methods -------------------'
+    yay -S fcitx-im fcitx-configtool fcitx-rime
+    yay -S fcitx-skins
+    #yay -S fcitx-sogoupinyin
+
+    # create .xprofile
+    #(
+        #echo '$a';
+        #echo 'export GTK_IM_MODULE=fcitx';
+        #echo 'export QT_IM_MODULE=fcitx';
+        #echo 'export XMODIFIERS="@im=fcitx"';
+        #echo '.';
+        #echo 'wq'
+    #) | ed -s ~/.xprofile
+
+    echo '----------------- Dolphin, kate -------------------'
+
+
+    echo '----------------- unrar -------------------'
+
+
+    echo '----------------- ranger (file manager)-------------------'
+    yay -S ranger
+    # Dependencies for ranger
+    # Syay -S atool mediainfo
+    # yay -S xsel
+
+    #echo '----------------- minidlna -------------------'
+    #sudo pacman -s  minidlna
+
+    # Psensor 检查CPU温度
+
+
+    # Logitech mouse
+    echo '----------------- xbindkeys -------------------'
+    yay -S xbindkeys
+
+    # Partition Tools
+    echo '----------------- gparted -------------------'
+
+    # Copy to Or paste to X clipboard
+    echo '----------------- xclip -------------------'
+    sudo pacman -S  xclip
+## }
+
+## Window Manager {
+    # i3-wm
+    #echo '----------------------- i3wm ---------------------------'
+    #yay -S i3-gaps
+
+    #echo '----------------------- bspwm ---------------------------'
+    yay -Sy sxhkd
+
+    # polybar - status bar tools
+    echo '----------------------- polybar ---------------------------'
+    yay -S polybar
+    yay -S alsa-utils
+    yay -S network-manager-applet
+
+    # picom(fork from compton), a compositor for Xorg
+    echo '----------------------- picom ---------------------------'
+    yay -S picom
+
+    # Wallpaper
+    yay -S feh
+
+    echo '----------------------- rofi ---------------------------'
+    yay -S rofi
+
+## }
+
+## Virtual Machine {
+    echo '----------------------- virtualbox ---------------------------'
+## }
+
+## 系统美化 {
+    # theme tools
+    echo '----------------------- Theme config tools ---------------------------'
+    #echo '----------------------- fonts ---------------------------'
+    # TODO 安装字体(i3,awesome font)
+    echo '----------------------- Icon ---------------------------'
+    echo '----------------------- Icon Fonts ---------------------------'
+    yay -S otf-font-awesome ttf-material-design-icons ttf-devicons vim-devicons
+    yay -S nerd-fonts-complete
+    echo '----------------------- powerline ---------------------------'
+    yay -S powerline powerline-fonts
+    yay -S noto-fonts-emoji
+    # font for code or terminal
+    yay -S ttf-dejavu otf-mplus ttf-meslo ttf-roboto
+    yay -S adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
+    #yay -S adobe-source-han-mono-otc-fonts
+    echo '----------------------- font manager ---------------------------'
+    yay -S font-manager
+## }
+
+## Entertainment
+    echo '----------------------- smplayer ---------------------------'
+    #echo '----------------------- kodi ---------------------------'
+    #yay -S kodi
+
+## Photo & Media
+    # echo '----------------------- Screen shot ---------------------------'
+    yay -S flameshot
+    # echo '----------------------- Darktable ---------------------------'
+    # yay -S darktable
+
+
+
+## Downloading Tools {
+## }
+
+
+## Developing tools {
+    echo '----------------- Developing Tools -------------------'
+    echo '----------------------- vim ---------------------------'
+    # vim doesn't support clipboard
+    #yay -S gvim
+    echo '----------------------- meld ---------------------------'
+    # diff tool to comparing files
+    yay -S meld
+    echo '----------------------- neovim ---------------------------'
+
+    # Java
+    # Eclipse (will install openjdk automatically)
+    #yay -S eclipse
+
+    # Python
+    #sudo apt -y install phantomjs
+    yay -S python-requests flake8 python-selenium python-beautifulsoup4 python-jedi autopep8 yapf
+    yay -S python2-pyquery python-pyquery
+
+    # nodejs
+    yay -S nodejs npm
+    # Error and problem detecting tool for Vim
+    #sudo npm install -g jshint
+    #sudo npm install -g tern
+    #yay -S nodejs-jshint nodejs-tern
+## }
+
+## Clone dotfiles from Git {
+    #echo '---------------------- myconfigs ---------------------'
+    #git clone https://github.com/blurm/myconfigs ~/git4install/myconfigs
+## }
+
+## Install plugins for Vim {
+    echo '---------------------- vim plugins ---------------------'
+    #cp ~/git4install/myconfigs_temp/{.vimrc,.viminfo} /home/damon
+    mkdir -p ~/.vim/autoload
+    cd ~/.vim/autoload
+    git clone https://github.com/blurm/damonvim.git
+    #vim +PluginInstall +qall
+
+    #echo '---------------------- YouCompleteMe ---------------------'
+    #cd ~/.vim/bundle/YouCompleteMe
+    #sudo ./install.py
+    #cd ~
+
+    #echo '---------------------- ropevim ---------------------'
+    #cd ~/.vim/bundle/ropevim
+    #sudo python setup.py install
+    #cd ~
+
+    #echo '---------------------- ctags for tagbar ---------------------'
+    #sudo apt -y install exuberant-ctags
+## }
+
+## Terminal Tools {
+    echo '--------------------- fuzzy search ----------------------------'
+    yay -S fzf ripgrep
+
+    echo '--------------------- cheat sheet ----------------------------'
+    yay -S cheat
+
+    echo '--------------------- urxvt and perl scripts ----------------------------'
+    yay -S rxvt-unicode-256xresources urxvt-clipboard urxvt-font-size-git
+
+    ## Need to put it at last, since chsh will interrupt the install process
+    echo '--------------------- zsh ----------------------------'
+    # Zsh
+    yay -S zsh
+    # on my zsh
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    echo '---------------------- zsh-syntax-highlighting ---------------------'
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    echo '---------------------- file path jumping ---------------------'
+    git clone https://github.com/skywind3000/z.lua ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/z.lua
+## }
