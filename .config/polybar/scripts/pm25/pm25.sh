36e7592f661458709ea25e2c8afa02673853f745
damon <blurm@126.com>
2020-03-19 16:58:14 +0800

    add pm25 module for polybar

--- .config/polybar/scripts/pm25/pm25.sh
+++ .config/polybar/scripts/pm25/pm25.sh
@@ -5,19 +5,22 @@ API_URL="https://api.waqi.info/feed/"
 
 data=$(curl -sf "${API_URL}${CITY}/?token=${KEY}")
 
-#if [ -n $data ]; then
+if [ -n "$data" ]; then
+    status=$(echo "$data" | jq -r ".status")
+    if [[ -n "$status" && $status = "ok" ]]; then
+        pm25=$(echo "$data" | jq -r ".data.iaqi.pm25.v")
+        pm10=$(echo "$data" | jq -r ".data.iaqi.pm10.v")
 
-
-pm25=$(echo "$data" | jq -r ".data.iaqi.pm25.v")
-pm10=$(echo "$data" | jq -r ".data.iaqi.pm10.v")
-
-if [ $pm10 -ge $pm25 ]; then
-    result=$pm10
-else
-    result=$pm25
+        if [ $pm10 -ge $pm25 ]; then
+            result=$pm10
+        else
+            result=$pm25
+        fi
+        echo "  $result"
+    else
+        result=""
+    fi
 fi
-
-echo "  $result"
 
 #https://api.waqi.info/feed/beijing/?token=f7794aaec722177e432098bfc06c29b51c4114bd
 
