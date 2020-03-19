d3f96117c4aa029a545b2160d6d8a3c79ea1925c
damon <blurm@126.com>
2020-03-19 16:36:01 +0800

    add pm25 module for polybar

---
+++ .config/polybar/scripts/pm25/pm25.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+KEY="f7794aaec722177e432098bfc06c29b51c4114bd"
+CITY="beijing"
+API_URL="https://api.waqi.info/feed/"
+
+data=$(curl -sf "${API_URL}${CITY}/?token=${KEY}")
+
+#if [ -n $data ]; then
+
+
+pm25=$(echo "$data" | jq -r ".data.iaqi.pm25.v")
+pm10=$(echo "$data" | jq -r ".data.iaqi.pm10.v")
+
+if [ $pm10 -ge $pm25 ]; then
+    result=$pm10
+else
+    result=$pm25
+fi
+
+echo "  $result"
+
+#https://api.waqi.info/feed/beijing/?token=f7794aaec722177e432098bfc06c29b51c4114bd
+
+
+
+
+
+
+
